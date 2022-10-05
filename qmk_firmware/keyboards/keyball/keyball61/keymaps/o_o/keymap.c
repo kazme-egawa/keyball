fd7b84f69cd1e9aed26a14a5179452ae8a2163aa
kazme-egawa <arnerican.f0otboy@gmail.com>
2022-10-05 18:29:42 +0900

    update keymap

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/o_o/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/o_o/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,              KC_RBRC, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_EQL   ,
-    KC_LALT  , KC_LGUI  , KC_SPC   , KC_SPC   , KC_LGUI  , MO(_UTIL), KC_SPC   ,              KC_ENT , KC_LGUI  , _______  , _______  , _______  , KC_GRV   , KC_BSLS 
+    KC_LALT  , KC_LGUI  , KC_SPC   , KC_SPC   , KC_LGUI  , MO(_UTIL), KC_SPC   ,              KC_ENT , KC_BSLS  , _______  , _______  , _______  , KC_GRV   , SCRL_MO 
   ),
     
   [_AUTO_MOUSE] = LAYOUT_universal(  
@@ -37,15 +37,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,              KC_RBRC, KC_N     , KC_M     , KC_BTN1  , KC_BTN3  , KC_BTN2  , KC_EQL   ,
-    KC_LALT  , KC_LGUI  , KC_SPC   , KC_SPC   , KC_LGUI  , MO(_UTIL), KC_SPC   ,              KC_ENT , KC_LGUI  , _______  , _______  , _______  , KC_GRV   , SCRL_MO
+    KC_LALT  , KC_LGUI  , KC_SPC   , KC_SPC   , KC_LGUI  , MO(_UTIL), KC_SPC   ,              KC_ENT , KC_BSLS  , _______  , _______  , _______  , KC_GRV   , SCRL_MO
   ),
 
   [_UTIL] = LAYOUT_universal(
     KC_ESC   , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    KC_TAB   , KC_Q     , S(KC_DEL), KC_W     , C(KC_K)  , _______  ,                                  _______  , KC_LBRC  , LSFT(KC_LBRC),LSFT(KC_RBRC),KC_RBRC,_______,
+    KC_TAB   , KC_Q     , S(KC_DEL), KC_W     , C(KC_K)  , SCRL_DVD ,                                  SCRL_DVI , KC_LBRC  , LSFT(KC_LBRC),LSFT(KC_RBRC),KC_RBRC,_______,
     KC_LCTL  , KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT , _______  ,                                  KC_HOME  , KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT , KC_END   ,
     KC_LSFT  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    , CPI_D100 ,            CPI_I100 , KBC_SAVE , KC_BTN4  , KC_BTN1  , KC_BTN3  , KC_BTN2  , KC_BTN5  ,
-    KC_LALT  , KC_LGUI  , KC_SPC   , KC_SPC   , KC_LGUI  , _______  , KC_SPC   ,            KC_ENT   , KC_DEL   , _______  , _______  , _______  , _______  , _______
+    KC_LALT  , KC_LGUI  , KC_SPC   , KC_SPC   , KC_LGUI  , _______  , KC_SPC   ,            KC_ENT   , KC_DEL   , _______  , _______  , _______  , _______  , SCRL_MO
   ),
 
   [_PREF] = LAYOUT_universal(
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // clang-format on
 
-#define MOUSE_TIMEOUT 1000
+#define MOUSE_TIMEOUT 30000
 deferred_token mouse_layer_token = INVALID_DEFERRED_TOKEN;
 
 uint32_t mouse_layer_cb(uint32_t trigger_time, void *cb_arg){
@@ -447,60 +447,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 
   switch (keycode)
   {
-    case KC_J:
-      if(get_mods() & MOD_MASK_GUI){
-        if(record->event.pressed){
-          tap_code(KC_LEFT);
-        }
-        return false;
-      }
-      break;
-    
-    case KC_K:
-      if(get_mods() & MOD_MASK_GUI){
-        if(record->event.pressed){
-          tap_code(KC_DOWN);
-        }
-        return false;
-      }
-      break;
-    
-    case KC_L:
-      if(get_mods() & MOD_MASK_GUI){
-        if(record->event.pressed){
-          tap_code(KC_UP);
-        }
-        return false;
-      }
-      break;
-    
-    case KC_SCLN:
-      if(get_mods() & MOD_MASK_GUI){
-        if(record->event.pressed){
-          tap_code(KC_RIGHT);
-        }
-        return false;
-      }
-      break;
-    
-    case KC_P:
-      if(get_mods() & MOD_MASK_GUI){
-        if(record->event.pressed){
-          tap_code(KC_6);
-        }
-        return false;
-      }
-      break;
-    
-    case KC_D:
-      if(get_mods() & MOD_MASK_GUI){
-        if(record->event.pressed){
-          tap_code(KC_5);
-        }
-        return false;
-      }
-      break;
-    
     /* KEYBOARD PET STATUS START */
 
     case KC_LCTL:
